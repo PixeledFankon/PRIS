86d6bdf879f0af7787e0813c2fafe8831e201003
PixeledFankon <kklounov702@gmail.com>
2026-02-23 15:00:18 +0500

    Очистка
    Теперь я собираюсь заниматься обучением модели. Для этого пришлось почистить все скрипты оставив только скрипт с классом героев. Также добавил эксель файл для создания экземпляров классов героев и обучения модели на них.

--- src/ClassHero.py
+++ src/ClassHero.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 @dataclass
 class ClassHero:
     Name: str
-    Faction: str        # blue, red, green
-    Role: str           # warrior, mage, marksman
+    Faction: str        # blue, red, green, purple
+    Class: str          # warrior, mage, marksman, defender
     AttackType: str     # melee, ranged, artillery
 
     def __str__(self):
-        return f"{self.Name} ({self.Faction}, {self.Role}, {self.AttackType})"
+        return f"{self.Name} ({self.Faction}, {self.Class}, {self.AttackType})"
